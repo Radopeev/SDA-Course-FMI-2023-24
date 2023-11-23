d4f69124a8db95033b76f053796b03ca69c87d88
Radopeev <95441540+Radopeev@users.noreply.github.com>
2023-11-23 20:28:45 +0200

    Update task2.cpp

--- homework_8/task2.cpp
+++ homework_8/task2.cpp
@@ -11,26 +11,24 @@ struct Lice {
     long pristigane;
     long trygvane;
     long stol=-1;
-  //chatichna naredba
     bool operator<(const Lice& other) const {
         if (pristigane == other.trygvane) {
             return trygvane < other.trygvane;
         }
-        return start < other.start;
+        return pristigane < other.pristigane;
     }
- //chatichna naredba
     bool operator>(const Lice& other) const {
-           return end > other.end;
+           return trygvane > other.trygvane;
     }
 };
 
 int main() {
     long n, t;
     cin >> n;
-    vector<Person> hora(n);
+    vector<Lice> hora(n);
     for (int i = 0; i < n; i++) {
         hora[i].index = i;
-        cin >> hora[i].start >> hora[i].end;
+        cin >> hora[i].pristigane >> hora[i].trygvane;
     }
     cin >> t;
     priority_queue<int,vector<int>, greater<int>> stolove;
@@ -38,33 +36,31 @@ int main() {
         stolove.push(i);
     }
     sort(hora.begin(), hora.end());
-    int result=0;
-    priority_queue<Person,vector<Person>,greater<Person>> sednali;
+    priority_queue<Lice,vector<Lice>,greater<Lice>> sednali;
     for (int i = 0; i < n; i++) {
-        if (sednali.empty() || hora[i].start < sednali.top().end) {
+        if (sednali.empty() || hora[i].pristigane < sednali.top().trygvane) {
             hora[i].stol=stolove.top();  
             stolove.pop();
             if (hora[i].index == t) {
-                result=hora[i].stol;
+                cout<<hora[i].stol;
                 break;
             }
             sednali.push(hora[i]);
         }
         else {
-            while (!sednali.empty() && sednali.top().end <= hora[i].start) {
+            while (!sednali.empty() && sednali.top().trygvane <= hora[i].pristigane) {
                 stolove.push(sednali.top().stol);
                 sednali.pop();
             }
             hora[i].stol=stolove.top();  
             stolove.pop();
             if (hora[i].index == t) {
-                result=hora[i].stol;
+                cout<<hora[i].stol;
                 break;
             }
             sednali.push(hora[i]);
         }
       
     }
-    cout<<result;
     return 0;
 }
