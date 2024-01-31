9634f733d5c70a8684c81a772e5ce11df9c482ac
Radopeev <95441540+Radopeev@users.noreply.github.com>
2024-01-31 10:32:55 +0200

    added tasks from exam 6

---
+++ Exam_6/23-24/task1.cpp
@@ -0,0 +1,48 @@
+﻿#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+using namespace std;
+
+int find(int v, vector<int>& parent) {
+    if (v == parent[v]) {
+        return v;
+    }
+    return parent[v] = find(parent[v], parent);
+}
+
+void connect(int u, int v, vector<int>& parent) {
+    parent[u] = parent[v];
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int x, y;
+    vector<int> parent(n + 1);
+    for (int i = 0; i <= n; i++) {
+        parent[i] = i;
+    }
+    unordered_map<int, vector<int>> graph;
+    int component = n;
+    while (cin >> x >> y) {
+        int u = find(x, parent);
+        int v = find(y, parent);
+        if (u != v) {
+            cout << --component << "\n";
+            connect(u, v, parent);
+        }
+        else {
+
+            cout << component << " CYCLE FORMED!\n";
+        }
+    }
+    return 0;
+}
+
+
+
+
+
